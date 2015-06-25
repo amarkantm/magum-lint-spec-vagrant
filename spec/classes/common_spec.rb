22aecfd77a3a20e733cab3f725a1c81b591955cf
AMAR KANT <amarkant4u@gmail.com>
2015-06-25 22:56:20 +0200

    added a few more spec tests

--- spec/classes/common_spec.rb
+++ spec/classes/common_spec.rb
@@ -2,6 +2,13 @@
 require_relative '../spec_helper'
 
 describe 'common', :type => :module do
+
+  context 'The catalog should at the very least compile' do
+    it { 
+         should compile
+    }
+  end
+
   context 'Checking for /var/rsi directory existance' do	
       it { should contain_file('/var/rsi').with(
         :ensure => 'directory',
@@ -10,5 +17,11 @@ describe 'common', :type => :module do
         :mode   => '0750',
          )
 }
+end
+  context 'The main class should be present in the catalog' do
+    it { 
+         should contain_class('common') 
+    }
+
 end
 end
